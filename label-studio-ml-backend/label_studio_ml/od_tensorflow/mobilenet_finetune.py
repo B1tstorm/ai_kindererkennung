454f3fb4400609ef7ef63de5c7aea44d4a3a51bd
B1tstorm <fokodiamond@gmail.com>
2023-01-04 14:52:33 +0100

    Docker startet mit eigenem OD model, probleme mit tensorflow oder object_detection package im docker

--- label-studio-ml-backend/label_studio_ml/od_tensorflow/mobilenet_finetune.py
+++ label-studio-ml-backend/label_studio_ml/od_tensorflow/mobilenet_finetune.py
@@ -1,94 +1,119 @@
 import os
 import logging
-import tensorflow as tf
-import tensorflow_hub as hub
-import numpy as np
+import pathlib
 
+import tensorflow as tf
+import numpy as np
+from object_detection.utils import label_map_util
 from PIL import Image
+
 from label_studio_ml.model import LabelStudioMLBase
-from label_studio_ml.utils import get_image_local_path, get_single_tag_keys, get_choice, is_skipped
+from label_studio_ml.utils import get_image_size, get_single_tag_keys, get_image_local_path
+from label_studio.core.utils.io import get_data_dir
+from label_studio.core.settings.base import DATA_UNDEFINED_NAME
+from urllib.parse import urlparse
 
 logger = logging.getLogger(__name__)
-feature_extractor_model = 'https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4'
 
 
 class TFMobileNet(LabelStudioMLBase):
 
-    def __init__(self, trainable=False, batch_size=32, epochs=3, **kwargs):
+    def __init__(self, image_dir=None, score_threshold=0.3, **kwargs):
+        """
+        :param image_dir: Directory where images are stored (leadve it default when you use direct file upload into Label Studio instead of URLs)
+        :param score_threshold: score threshold to wipe out noisy results
+        :param kwargs:
+        """
         super(TFMobileNet, self).__init__(**kwargs)
 
-        self.image_width, self.image_height = 224, 224
-        self.trainable = trainable
-        self.batch_size = batch_size
-        self.epochs = epochs
+        self.image_dir = image_dir
 
-        self.feature_extractor_layer = hub.KerasLayer(
-            feature_extractor_model,
-            input_shape=(self.image_width, self.image_height, 3),
-            trainable=trainable)
+        # Load the exported model from saved_model directory
+        PATH_TO_SAVED_MODEL = '/saved_models/centernet_hg104_1024x1024_coco17_tpu-32'
+        print("Loading model...")
+        # Lead saved model and build detection fuction
+        self.detect_fn = tf.saved_model.load(PATH_TO_SAVED_MODEL)
+        print('Model loading done....')
+
+        # Load label map data
+        def download_labels(filename):
+            base_url = 'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/data/'
+            label_dir = tf.keras.utils.get_file(fname=filename,
+                                                origin=base_url + filename,
+                                                untar=False)
+            label_dir = pathlib.Path(label_dir)
+            print("#########" + str(label_dir))
+
+            return str(label_dir)
+
+        LABEL_FILENAME = 'mscoco_label_map.pbtxt'
+        PATH_TO_LABELS = download_labels(LABEL_FILENAME)
+
+        self.category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
 
         self.from_name, self.to_name, self.value, self.labels_in_config = get_single_tag_keys(
-            self.parsed_label_config, 'Choices', 'Image')
-        self.labels = tf.convert_to_tensor(sorted(self.labels_in_config))
-        num_classes = len(self.labels_in_config)
-        self.model = tf.keras.Sequential([
-            self.feature_extractor_layer,
-            tf.keras.layers.Dense(num_classes)
-        ])
-        self.model.summary()
-        if self.train_output:
-            model_file = self.train_output['model_file']
-            logger.info('Restore model from ' + model_file)
-            # Restore previously saved weights
-            self.model.load_weights(self.train_output['model_file'])
+            self.parsed_label_config, 'RectangleLabels', 'Image')
+
+        # Create a list of labels from the label_config. We use category_index instead.
+        # schema = list(self.parsed_label_config.values())[0]
+        # self.labels_in_config = set(self.labels_in_config)
+
+        self.score_thresh = score_threshold
+
+    def _get_image_url(self, task):  # 'data' stores the fullpath of the img
+        image_url = task['data'].get(self.value) or task['data'].get(DATA_UNDEFINED_NAME)
+
+        return image_url
+
+    def load_image_into_numpy_array(self, path):
+        return np.array(Image.open(path))
 
     def predict(self, tasks, **kwargs):
-        image_path = get_image_local_path(tasks[0]['data'][self.value])
+        assert len(tasks) == 1
+        task = tasks[0]  # looks like task (img) is dealt one by one
+        image_url = self._get_image_url(task)
+        print('image_url:', image_url)
+        image_path = self.get_local_path(image_url, project_dir=self.image_dir)
+        print('image_path:', image_path)
 
-        image = Image.open(image_path).resize((self.image_width, self.image_height))
-        image = np.array(image) / 255.0
-        result = self.model.predict(image[np.newaxis, ...])
-        predicted_label_idx = np.argmax(result[0], axis=-1)
-        predicted_label_score = result[0][predicted_label_idx]
-        predicted_label = self.labels[predicted_label_idx]
+        image_np = self.load_image_into_numpy_array(image_path)
+        input_tensor = tf.convert_to_tensor(image_np)
+        input_tensor = input_tensor[tf.newaxis, ...]
+        detections = self.detect_fn(input_tensor)
+
+        # All outputs are batches tensors.
+        # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+        # Only interested in the first num_detections.
+
+        boxes = detections['detection_boxes'][0].numpy()  # Getting the list of box coordinates
+        max_boxes_to_draw = boxes.shape[0]  # Getting the number of the list
+        scores = detections['detection_scores'][0].numpy()  # Getting scores for threshold evaluation
+
+        results = []
+        all_scores = []
+
+        img_width, img_height = get_image_size(image_path)  # This is not used either
+
+        for i in range(boxes.shape[0]):
+            if scores is None or scores[i] > self.score_thresh:
+                class_name = self.category_index[detections['detection_classes'][0, i].numpy().astype(int)]['name']
+                results.append({
+                    "from_name": self.from_name,
+                    "to_name": self.to_name,
+                    "type": "rectanglelabels",
+                    "value": {
+                        "rectanglelabels": [class_name],
+                        "x": boxes[i, 1] * 100,
+                        "y": boxes[i, 0] * 100,
+                        "width": (boxes[0, 3] - boxes[0, 1]) * 100,
+                        "height": (boxes[0, 2] - boxes[0, 0]) * 100
+                    }
+                })
+                all_scores.append(scores[i])
+        avg_score = sum(all_scores) / max(len(all_scores), 1)
+        print("results", results)
+        print("each score:", all_scores)
         return [{
-            'result': [{
-                'from_name': self.from_name,
-                'to_name': self.to_name,
-                'type': 'choices',
-                'value': {'choices': [str(predicted_label.numpy(), 'utf-8')]}
-            }],
-            'score': float(predicted_label_score)
+            "result": results,
+            "score": avg_score
         }]
-
-    def fit(self, completions, workdir=None, **kwargs):
-
-        annotations = []
-        for completion in completions:
-            if is_skipped(completion):
-                continue
-            image_path = get_image_local_path(completion['data'][self.value])
-            image_label = get_choice(completion)
-            annotations.append((image_path, image_label))
-
-        # Create dataset
-        ds = tf.data.Dataset.from_tensor_slices(annotations)
-
-        def prepare_item(item):
-            label = tf.argmax(item[1] == self.labels)
-            img = tf.io.read_file(item[0])
-            img = tf.image.decode_jpeg(img, channels=3)
-            img = tf.image.resize(img, [self.image_height, self.image_width])
-            return img, label
-
-        ds = ds.map(prepare_item, num_parallel_calls=tf.data.AUTOTUNE)
-        ds = ds.cache().shuffle(buffer_size=1000).batch(self.batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
-
-        self.model.compile(
-            optimizer=tf.keras.optimizers.Adam(),
-            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-            metrics=['acc'])
-        self.model.fit(ds, epochs=self.epochs)
-        model_file = os.path.join(workdir, 'checkpoint')
-        self.model.save_weights(model_file)
-        return {'model_file': model_file}
